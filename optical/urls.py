90a724966c1284d7faadf2b50b24f9d1c50bc099
John Guo <zhichenguo@gmail.com>
2020-04-10 00:52:32 -0400

    Update Models, Views, Forms, HTML Files

--- optical/urls.py
+++ optical/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from .views import ProductListView, ProductDetailView
-from .views import product_create, product_update, product_delete
+from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = 'optical'
 
@@ -9,14 +8,18 @@ urlpatterns = [
     path('products/', ProductListView.as_view(), name='product_list'),
     # path('products/', product_list, name='product_list'),
 
-    path('products/create/', product_create),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    # path('products/create/', product_create),
     # id is the django default id, if model has other pk_name should use it
     # add '/' at the end! without / still work. If don't add '/' at the end, without / doesn't work
 
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     # path('products/<int:id>/', product_detail),
 
-    path('products/<int:id>/update/', product_update),
-    path('products/<int:id>/delete/', product_delete),
+    path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    # path('products/<int:id>/update/', product_update),
+
+    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+    # path('products/<int:id>/delete/', product_delete),
 
 ]
--- optical/views.py
+++ optical/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
 
 from .forms import ProductForm
 from .models import Product
@@ -49,6 +50,7 @@ class ProductDetailView(DetailView):
 
 
 def product_detail(request, id):
+    # same as ProductDetailView
     product = Product.objects.get(id=id)
     context = {
         "product": product
@@ -56,8 +58,33 @@ def product_detail(request, id):
     return render(request, 'product_detail.html', context)
 
 
+class ProductCreateView(CreateView):
+    template_name = 'product_create.html'
+    model = Product
+    fields = ['name', 'category', 'description', 'price', 'inventory', 'image']
+    success_url = '/products/'
+
+    def form_valid(self, form):
+        form.save()
+        # do something like send email
+        return super().form_valid(form)
+
+
+# class ProductCreateView(FormView):
+#     # same as above ProductCreateView with CreateForm
+#     template_name = 'product_create.html'
+#     form_class = ProductForm
+#     success_url = '/products/'
+#
+#     def form_valid(self, form):
+#         form.save()
+#         # do something like send email
+#         return super().form_valid(form)
+
+
 def product_create(request):
-    form = ProductForm(request.POST or None)
+    # same as ProductCreateView
+    form = ProductForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         # print(form.cleaned_data)
         # new_product = Product.objects.create(
@@ -73,10 +100,41 @@ def product_create(request):
     return render(request, 'product_create.html', context)
 
 
+class ProductUpdateView(UpdateView):
+    template_name = 'product_update.html'
+    model = Product
+    fields = ['name', 'category', 'description', 'price', 'inventory', 'image']
+    success_url = '..'
+
+    def form_valid(self, form):
+        form.instance.event_agency = self.request.user.pk
+        form.save()
+        return super().form_valid(form)
+
+
+# class ProductUpdateView(FormView):
+#     # same as above ProductUpdateView with UpdateForm
+#     template_name = 'product_update.html'
+#     form_class = ProductForm
+#     success_url = '..'
+#
+#     def get_form_kwargs(self):
+#         form_kwargs = super(ProductUpdateView, self).get_form_kwargs()
+#         if 'pk' in self.kwargs:
+#             form_kwargs['instance'] = Product.objects.get(pk=int(self.kwargs['pk']))
+#         return form_kwargs
+#
+#     def form_valid(self, form):
+#         # form.instance = self.get_object
+#         form.save()
+#         return super().form_valid(form)
+
+
 def product_update(request, id):
+    # same as ProductUpdateView
     product = Product.objects.get(id=id)
     # instance put the old info into the form
-    form = ProductForm(request.POST or None, instance=product)
+    form = ProductForm(request.POST or None, request.FILES or None, instance=product)
     if form.is_valid():
         form.save()
         # return redirect('/products/' + str(id) + '/')
@@ -85,7 +143,14 @@ def product_update(request, id):
     return render(request, 'product_update.html', context)
 
 
+class ProductDeleteView(DeleteView):
+    template_name = 'product_confirm_delete.html'
+    model = Product
+    success_url = '/products/'
+
+
 def product_delete(request, id):
+    # same as ProductDeleteView
     product = Product.objects.get(id=id)
     product.delete()
     # return redirect('/products/')  # same below

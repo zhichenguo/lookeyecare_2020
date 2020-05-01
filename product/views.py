92d86a2955e61e0c335a30577b9c9e8446664418
Zhichen Guo <zhichenguo@gmail.com>
2020-05-01 15:53:58 -0400

    Search Bar

--- product/views.py
+++ product/views.py
@@ -1,9 +1,11 @@
+from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import DeleteView, UpdateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from .forms import ProductForm
-from .models import Product
+from .models import Product, CATEGORY_CHOICES
 
 
 class ProductListView(ListView):
@@ -13,6 +15,43 @@ class ProductListView(ListView):
 
     paginate_by = 6
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.object_list = self.get_queryset()
+
+    def get(self, request, *args, **kwargs):
+        categories = [row[1] for row in CATEGORY_CHOICES]
+        search_text = request.GET.get('search_text')
+        # category = request.GET.get('category')
+        women = request.GET.get('women')
+        men = request.GET.get('men')
+        kid = request.GET.get('kid')
+        sun = request.GET.get('sun')
+        onsale = request.GET.get('onSale')
+        print(categories, women, men, kid, sun, onsale, search_text)
+
+        if search_text != '' and search_text is not None:
+            self.object_list = self.object_list.filter(
+                Q(name__icontains=search_text) | Q(description__icontains=search_text)).distinct()
+
+        # if category != '' and category is not None and category != 'Choose...':
+        #     self.object_list = self.object_list.filter(category=category)
+
+        if women == 'on':
+            objs_search_women = self.object_list.filter(category='W')
+        if men == 'on':
+            objs_search_men = self.object_list.filter(category='M')
+        if kid == 'on':
+            objs_search_kid = self.object_list.filter(category='K')
+        if sun == 'on':
+            objs_search_sun = self.object_list.filter(category='S')
+        if onsale == 'on':
+            self.object_list = self.object_list.filter(label='S')
+
+        context = self.get_context_data()
+
+        return self.render_to_response(context)
+
     # auto html file is 'app_name (product)' / 'model_name'(product) _list
     # the context passed in will be "object_list"
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -20,6 +59,7 @@ class ProductListView(ListView):
         context = super().get_context_data(**kwargs)
         # can add anything else in the context here
         # context['addition_var'] = addition_value
+        context['categories'] = CATEGORY_CHOICES
         return context
 
     def get_queryset(self):
